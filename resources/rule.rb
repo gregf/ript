d70205d6a453a2b0d9820ecdfd8f855c6567b47a
Greg Fitzgerald <greg@gregf.org>
2013-09-01 15:15:19 -0400

    make sure ript is a block

--- resources/rule.rb
+++ resources/rule.rb
@@ -2,4 +2,12 @@ actions :add, :delete
 default_action :add
 
 attribute :group, :kind_of => [String, Symbol], :default => 'default'
-attribute :ript, :kind_of => Proc
+attribute :ript, :kind_of => Proc, :required => true
+
+def ript(&block)
+  if block_given? and ript
+    @ript = ript
+  else
+    @ript
+  end
+end
