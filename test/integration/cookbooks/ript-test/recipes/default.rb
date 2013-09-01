2e6aa0931346dc99226f394c161a4930f39496ba
Greg Fitzgerald <greg@gregf.org>
2013-09-01 14:20:20 -0400

    Add tests for the default recipe

--- test/integration/cookbooks/ript-test/recipes/default.rb
+++ test/integration/cookbooks/ript-test/recipes/default.rb
@@ -5,13 +5,13 @@
 
 include_recipe 'ript'
 
-ript_rule 'allow ssh from office' do
-  ript do
-    label 'office', :address => '127.0.0.121'
-    accept 'office' do
-      from 'office'
-      ports 22
-    end
-  end
-end
-
+# ript_rule 'allow ssh from office' do
+#   ript do
+#     label 'office', :address => '127.0.0.121'
+#     accept 'office' do
+#       from 'office'
+#       ports 22
+#     end
+#   end
+# end
+# 
