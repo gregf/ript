f5ffef5e681647ef21783a25794b615c75e40007
Greg Fitzgerald <greg@gregf.org>
2013-09-01 13:16:22 -0400

    Cleanup metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,15 @@
-name 'ript'
-maintainer 'Chris Roberts'
-maintainer_email 'chrisroberts.code@gmail.com'
-version '0.1.0'
+name              'ript'
+maintainer        'Chris Roberts'
+maintainer_email  'chrisroberts.code@gmail.com'
+license           ' '
+description       'Provides a lwrp for working with the ript gem.'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
+
+%w{ubuntu debian centos}.each do |os|
+  supports os
+end
 
 depends 'chef_package'
+
+recipe "default", "Installs the ript gem"
